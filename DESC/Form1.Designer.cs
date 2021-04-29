54028f59677db447ade868f2290189844b7e0694
sai wanza <q020385791@gmail.com>
2021-04-29 16:24:53 +0800

    加入專案檔案。

---
+++ DESC/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace DESC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(88, 35);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(237, 29);
+            this.txtInput.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(88, 103);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(237, 29);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "輸入";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "結果";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(104, 185);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(84, 32);
+            this.btnEncrypt.TabIndex = 5;
+            this.btnEncrypt.Text = "加密";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(238, 185);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(78, 32);
+            this.btnDecrypt.TabIndex = 6;
+            this.btnDecrypt.Text = "解密";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 247);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "DESC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ DESC/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DESC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            Security Sec = new Security();
+            txtResult.Text =Sec.Encrypt(txtInput.Text);
+        }
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Security Sec = new Security();
+            txtResult.Text = Sec.Decrypt(txtInput.Text, Sec._Key,Sec._IV);
+        }
+
+        public class Security 
+        {
+            public string _Key;
+            public string _IV;
+
+
+            public string Key 
+            {
+                set 
+                {
+                    _Key = value.Length == 8 ? value : "!@#$%^&*";
+                }
+            }
+
+            public string IV 
+            {
+                set 
+                {
+                    _IV = value.Length == 8 ? value : ")(*&^%$#";
+                }
+            }
+            public Security()
+            {
+                _Key = "!@#$%^&*";
+                _IV = ")(*&^%$#";
+
+            }
+            public Security(string newKey, string newIV)
+            {
+                this.Key = newKey;
+                this.IV = newIV;
+            }
+            public string Encrypt(string value)
+            {
+                return Encrypt(value, _Key, _IV);
+            }
+            private string Encrypt(string pToEncrypt, string sKey, string sIV) 
+            {
+                StringBuilder ret = new StringBuilder();
+                //字元轉換為Byte
+              
+
+                using (DESCryptoServiceProvider des=new DESCryptoServiceProvider())
+                {
+                    byte[] inputByteArray = Encoding.Default.GetBytes(pToEncrypt);
+                    //加密金鑰
+                    des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                    des.IV= ASCIIEncoding.ASCII.GetBytes(sIV);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inputByteArray, 0, inputByteArray.Length);
+                            cs.FlushFinalBlock();
+                        }
+                        //輸出資料
+                        foreach (byte b in ms.ToArray())
+                            ret.AppendFormat("{0:X2}", b);
+                    }
+
+                }
+                return ret.ToString();
+            }
+
+            public string Decrypt(string pToDecrypt, string sKey, string sIV) 
+            {
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    byte[] inputByteArray = new byte[pToDecrypt.Length / 2];
+                    //反轉
+                    for (int x = 0; x < pToDecrypt.Length / 2; x++)
+                    {
+                        int i = (Convert.ToInt32(pToDecrypt.Substring(x * 2, 2), 16));
+                        inputByteArray[x] = (byte)i;
+                    }
+                    des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                    des.IV = ASCIIEncoding.ASCII.GetBytes(sIV);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            //例外處理
+                            try
+                            {
+                                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                                cs.FlushFinalBlock();
+                                //輸出資料
+                                return System.Text.Encoding.Default.GetString(ms.ToArray());
+                            }
+                            catch (CryptographicException)
+                            {
+                                return "N/A";
+                            }
+                        }
+                    }
+                }
+
+            }
+            public bool ValidateString(string EnString, string FoString)
+            {
+                //呼叫Decrypt解密
+                //判斷是否相符
+                //回傳結果
+                return Decrypt(EnString, _Key, _IV) == FoString.ToString() ? true : false;
+            }
+        }
+
+       
+    }
+}
